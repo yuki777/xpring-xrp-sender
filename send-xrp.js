976ecb1de91daf29f46986cec82208490e866105
9kmmr <tunglam195@gmail.com>
2021-12-16 15:29:56 +0700

    change xpring-js to xrpl

--- send-xrp.js
+++ send-xrp.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const moment = require('moment')
 const program = require('commander')
-const { Wallet, XrpClient, XrplNetwork, XrpUtils } = require("xpring-js");
+
+const xrpl = require("xrpl");
 
 program.on('--help', function(){
   console.log('')
   console.log('Examples:')
-  console.log('  node send-xrp.js -d rUas92gJndsSWhFCBbkH3N1yt8YztVfosA -t 4294967295 -a 0.000001')
+  console.log('  node send-xrp.js -d rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh -t 105267367 -a 0.000001')
 })
 program
-  .version('0.9.0')
-  .option('-d, --destination <destination>', 'Destination xrp address', /^r[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{24,34}$/, false)
+  .version('1.0.0')
+  .option('-d, --destination <destination>', 'Destination xrp address', /^r[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{24,34}$/, false)  
   .option('-t, --tag [tag]', 'Destination tag', /^[0-9]{1,10}$/, null)
   .option('-a, --amount <amount>', 'Send amount. 1 = 1 XRP.', parseFloat, false)
 
@@ -20,36 +21,43 @@ if(program.tag === false) program.help();
 if(program.amount === false) program.help();
 
 const receiverClassicAddress = program.destination;
-const receiverTag = program.tag;
+const tag = program.tag;
 const amount = program.amount;
 
 async function send(){
-  const senderWallet = Wallet.generateWalletFromSeed(process.env.SENDER_SEED);
+  const senderWallet =    xrpl.Wallet.fromSeed(process.env.SENDER_SEED);
   const remoteURL = process.env.REMOTE_URL;
-  const drop = BigInt((Math.round(amount*1000000)).toString()); // 1XRP = 1,000,000drop
-
-  let testOrMain = XrplNetwork.Main;
-  if(process.env.IS_TESTNET){
-    testOrMain = XrplNetwork.Test;
-  }
-
-  const xrpClient = new XrpClient(remoteURL, testOrMain);
-  const senderXAddress = senderWallet.getAddress();
-  const senderClassicAddress = XrpUtils.decodeXAddress(senderXAddress);
-  const receiverXAddress = XrpUtils.encodeXAddress(receiverClassicAddress, receiverTag);
-
+ 
+  const client = new xrpl.Client(remoteURL);
+  await client.connect();
+  // Prepare transaction -------------------------------------------------------
+  const prepared = await client.autofill({
+    "TransactionType": "Payment",
+    "Account": senderWallet.address,
+    "Amount": xrpl.xrpToDrops(amount),
+    "Destination": receiverClassicAddress,
+    "DestinationTag": Number(tag)
+  })
+  const max_ledger = prepared.LastLedgerSequence
   console.log("Datetime : " + moment().format());
-  console.log("Sender X Address : " + senderXAddress);
-  console.log("Sender Classic Address : " + senderClassicAddress.address);
-  console.log("Sender Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + senderClassicAddress.address);
-  console.log("Send Amount (XRP) : " + amount);
-  console.log("Send Amount (drop) : " + drop);
-  console.log("Receiver X Address : " + receiverXAddress);
-  console.log("Receiver Classic Address : " + receiverClassicAddress);
-  console.log("Receiver Classic Address Tag : " + receiverTag);
-  console.log("Receiver Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + receiverClassicAddress + "#" + receiverTag);
+  console.log("Prepared transaction instructions:", prepared)
+  console.log("Transaction cost:", xrpl.dropsToXrp(prepared.Fee), "XRP")
+  console.log("Transaction expires after ledger:", max_ledger)
+  console.log("Sender X Address : " + senderWallet.address);    
+  console.log("Send Amount (XRP) : " + amount);  
+  console.log("Receiver X Address : " + receiverClassicAddress);
+  
+  // Sign prepared instructions ------------------------------------------------
+  const signed = senderWallet.sign(prepared)
+  console.log("Identifying hash:", signed.hash)
+  console.log("Signed blob:", signed.tx_blob)
+  // Submit signed blob --------------------------------------------------------
+  const tx = await client.submitAndWait(signed.tx_blob)
+  // Check transaction results -------------------------------------------------
+  console.log("Transaction result:", tx.result.meta.TransactionResult)
+  console.log("Balance changes:", JSON.stringify(xrpl.getBalanceChanges(tx.result.meta), null, 2))
+  // Disconnect when done (If you omit this, Node.js won't end the process)
+  client.disconnect()
 
-  const transactionHash = await xrpClient.send(drop, receiverXAddress, senderWallet);
-  console.log("Transaction : " + process.env.EXPLORER_URL + "/transactions/" + transactionHash);
 }
 send();
