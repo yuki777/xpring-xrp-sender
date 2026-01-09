b506636e2fb09c0df85c066ee8b26f508fff006e
Yuki Adachi <yuki777@gmail.com>
2026-01-09 08:36:51 +0700

    Use xrpl

--- readme.md
+++ readme.md
@@ -1,16 +1,24 @@
-# Install
-```
+# send-xrp.js
+
+## Install
+
+```bash
 npm install
 ```
 
-# Send XRP on Testnet
-```
+## Send XRP on Testnet
+
+```bash
+# Create address on Testnet
+curl -s -X POST 'https://faucet.altnet.rippletest.net/accounts' | jq
+
 cp .env.testnet .env # => Edit .env
 node send-xrp.js --help
 ```
 
-# Send XRP on Mainnet
-```
+## Send XRP on Mainnet
+
+```bash
 cp .env.mainnet .env # => Edit .env
 node send-xrp.js --help
 ```
--- send-xrp.js
+++ send-xrp.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const moment = require('moment')
-const program = require('commander')
-const { Wallet, XrpClient, XrplNetwork, XrpUtils } = require("xpring-js");
+const dayjs = require('dayjs')
+const { program } = require('commander')
+const { Wallet, Client, xrpToDrops, dropsToXrp } = require('xrpl')
 
 program.on('--help', function(){
   console.log('')
@@ -9,47 +9,72 @@ program.on('--help', function(){
   console.log('  node send-xrp.js -d rUas92gJndsSWhFCBbkH3N1yt8YztVfosA -t 4294967295 -a 0.000001')
 })
 program
-  .version('0.9.0')
-  .option('-d, --destination <destination>', 'Destination xrp address', /^r[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{24,34}$/, false)
-  .option('-t, --tag [tag]', 'Destination tag', /^[0-9]{1,10}$/, null)
-  .option('-a, --amount <amount>', 'Send amount. 1 = 1 XRP.', parseFloat, false)
+  .version('1.0.0')
+  .option('-d, --destination <destination>', 'Destination xrp address')
+  .option('-t, --tag [tag]', 'Destination tag')
+  .option('-a, --amount <amount>', 'Send amount. 1 = 1 XRP.')
 
 program.parse(process.argv);
-if(program.destination === false) program.help();
-if(program.tag === false) program.help();
-if(program.amount === false) program.help();
+const options = program.opts();
 
-const receiverClassicAddress = program.destination;
-const receiverTag = program.tag;
-const amount = program.amount;
+if(!options.destination || !options.destination.match(/^r[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{24,34}$/)) {
+  program.help();
+}
+if(options.amount === undefined || isNaN(parseFloat(options.amount))) {
+  program.help();
+}
+
+const receiverClassicAddress = options.destination;
+const receiverTag = options.tag ? parseInt(options.tag) : undefined;
+const amount = parseFloat(options.amount);
 
 async function send(){
-  const senderWallet = Wallet.generateWalletFromSeed(process.env.SENDER_SEED);
+  const senderWallet = Wallet.fromSeed(process.env.SENDER_SEED);
   const remoteURL = process.env.REMOTE_URL;
-  const drop = BigInt((Math.round(amount*1000000)).toString()); // 1XRP = 1,000,000drop
+  const drops = xrpToDrops(amount);
 
-  let testOrMain = XrplNetwork.Main;
-  if(process.env.IS_TESTNET){
-    testOrMain = XrplNetwork.Test;
+  const client = new Client(remoteURL);
+  await client.connect();
+
+  const senderClassicAddress = senderWallet.classicAddress;
+
+  console.log("Datetime : " + dayjs().format());
+  console.log("Sender Classic Address : " + senderClassicAddress);
+  console.log("Sender Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + senderClassicAddress);
+  console.log("Send Amount (XRP) : " + amount);
+  console.log("Send Amount (drop) : " + drops);
+  console.log("Receiver Classic Address : " + receiverClassicAddress);
+  if(receiverTag !== undefined) {
+    console.log("Receiver Destination Tag : " + receiverTag);
+    console.log("Receiver Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + receiverClassicAddress + "#" + receiverTag);
+  } else {
+    console.log("Receiver Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + receiverClassicAddress);
   }
 
-  const xrpClient = new XrpClient(remoteURL, testOrMain);
-  const senderXAddress = senderWallet.getAddress();
-  const senderClassicAddress = XrpUtils.decodeXAddress(senderXAddress);
-  const receiverXAddress = XrpUtils.encodeXAddress(receiverClassicAddress, receiverTag);
+  const payment = {
+    TransactionType: 'Payment',
+    Account: senderClassicAddress,
+    Destination: receiverClassicAddress,
+    Amount: drops
+  };
 
-  console.log("Datetime : " + moment().format());
-  console.log("Sender X Address : " + senderXAddress);
-  console.log("Sender Classic Address : " + senderClassicAddress.address);
-  console.log("Sender Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + senderClassicAddress.address);
-  console.log("Send Amount (XRP) : " + amount);
-  console.log("Send Amount (drop) : " + drop);
-  console.log("Receiver X Address : " + receiverXAddress);
-  console.log("Receiver Classic Address : " + receiverClassicAddress);
-  console.log("Receiver Classic Address Tag : " + receiverTag);
-  console.log("Receiver Address Explorer : " + process.env.EXPLORER_URL + "/accounts/" + receiverClassicAddress + "#" + receiverTag);
+  if(receiverTag !== undefined) {
+    payment.DestinationTag = receiverTag;
+  }
 
-  const transactionHash = await xrpClient.send(drop, receiverXAddress, senderWallet);
-  console.log("Transaction : " + process.env.EXPLORER_URL + "/transactions/" + transactionHash);
+  try {
+    const prepared = await client.autofill(payment);
+    const signed = senderWallet.sign(prepared);
+    const result = await client.submitAndWait(signed.tx_blob);
+
+    console.log("Transaction Hash : " + signed.hash);
+    console.log("Transaction : " + process.env.EXPLORER_URL + "/transactions/" + signed.hash);
+    console.log("Result : " + result.result.meta.TransactionResult);
+  } catch (error) {
+    console.error("Transaction failed:", error.message);
+  } finally {
+    await client.disconnect();
+  }
 }
+
 send();
